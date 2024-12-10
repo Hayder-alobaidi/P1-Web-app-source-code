8f87dd4ad1496d70d3dd1dc9817e0e30c2aaa73e
Hayder Alobaidi <93683931+Hayder-alobaidi@users.noreply.github.com>
2024-12-09 16:25:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Hayder after the update'
+    return 'Hello from Hayder after the update haha'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
